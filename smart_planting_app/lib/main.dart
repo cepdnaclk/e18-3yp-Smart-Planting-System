4a82d03f77fd42e1c8d389c0ab845d73c4e25987
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-05 17:09:39 +0530

    mobile app update

---
+++ smart_planting_app/lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'screens/landingPage.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: landingScreen(),
+    );
+  }
+}
---
+++ smart_planting_app/lib/screens/landingPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class landingScreen extends StatelessWidget {
+  const landingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BottomAppBar(
+        color: Colors.green,
+      ),
+    );
+  }
+}

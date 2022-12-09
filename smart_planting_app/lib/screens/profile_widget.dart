450015963d9191f33a3433abd402f437cfaf8ee3
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-09 14:26:22 +0530

    Create profile_widget.dart

---
+++ smart_planting_app/lib/screens/profile_widget.dart
@@ -0,0 +1,68 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileWidget extends StatelessWidget {
+  const profileWidget({Key? key}) : super(key: key);
+
+  final File image;
+  final ValueChanged<ImageSource> onClicked;
+
+  const ProfileWidget (
+  {
+    Key? key,
+    required this.image,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+
+    return Center(
+      child: Stack(
+        children: [
+          buildImage(context),
+          Positioned(
+              bottom: 0,
+              right: 4,
+              child: buildEdition(color)
+          )
+        ],
+      ),
+    );
+  }
+  
+  Widget buildImage(BuildContext context) {
+    final imagePath = this.image,path;
+    final Object image;
+    if (imagePath.contains('https://')) {
+      image = NetworkImage(imagePath);
+    } else {
+      image = FileImage(File(imagePath));
+    }
+    
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent,
+        child: Ink.image(
+          image: image as ImageProvider,
+          fit: BoxFit.cover,
+          width: 128,
+          height: 128,
+          child: InkWell(
+              onTap: () async {
+                final source = await showImageSource(context);
+                if (source == null) return;
+              }
+          ),
+        ),
+      ),
+    );
+  }
+
+ Widget buildEdition(Color color) {
+
+ }
+}

bba97754476eed8a5ddebf77e2e37509423f26a8
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-09 15:46:35 +0530

    Update profile_widget.dart

--- smart_planting_app/lib/screens/profile_widget.dart
+++ smart_planting_app/lib/screens/profile_widget.dart
@@ -1,20 +1,13 @@
-import 'dart:html';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileWidget extends StatelessWidget {
-  const profileWidget({Key? key}) : super(key: key);
-
   final File image;
   final ValueChanged<ImageSource> onClicked;
 
-  const ProfileWidget (
-  {
-    Key? key,
-    required this.image,
-    required this.onClicked,
-  }) : super(key: key);
+  const ProfileWidget({Key? key, required this.image, required this.onClicked}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +20,7 @@ class ProfileWidget extends StatelessWidget {
           Positioned(
               bottom: 0,
               right: 4,
-              child: buildEdition(color)
+              child: buildEditIcon(color)
           )
         ],
       ),
@@ -35,8 +28,8 @@ class ProfileWidget extends StatelessWidget {
   }
   
   Widget buildImage(BuildContext context) {
-    final imagePath = this.image,path;
-    final Object image;
+    final imagePath = this.image.path;
+    final image;
     if (imagePath.contains('https://')) {
       image = NetworkImage(imagePath);
     } else {
@@ -62,8 +55,31 @@ class ProfileWidget extends StatelessWidget {
     );
   }
 
- Widget buildEdition(Color color) {
+ Widget buildEditIcon(Color color) => buildCircle(
+   color: Colors.white,
+   all: 3,
+   child: buildCircle(
+     color: color,
+     all: 8,
+     child: Icon(
+       Icons.edit,
+       size: 20,
+     ),
+   ),
+ );
 
- }
+ Widget buildCircle({
+   required Color color,
+   required double all,
+   required Widget child}) =>
+     ClipOval(
+       child: Container(
+         padding: EdgeInsets.all(all),
+         color: color,
+         child: child,
+       ),
+     );
+
+  showImageSource(BuildContext context) {}
 
 }

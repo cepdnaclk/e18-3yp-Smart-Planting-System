b6724867a5c85c68b19631050a93570d87cba90a
anushanga pavith <anushangapavith2@gmail.com>
2022-12-11 15:17:06 +0530

    Created new database

---
+++ Server/Database/database.sql
@@ -0,0 +1,55 @@
+-- Create the database
+CREATE DATABASE smart_planting_system;
+
+USE smart_planting_system;
+
+CREATE TABLE IF NOT EXISTS user_table(
+    userID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    mobileNo INT,
+    joinDate DATETIME,
+    profileImg LONGBLOB
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE IF NOT EXISTS password_table(
+    userID INT NOT NULL PRIMARY KEY,
+    userPassword VARCHAR(100) NOT NULL,
+    createDate DATETIME
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE IF NOT EXISTS plant_owner_table(
+    plantID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userID INT NOT NULL,
+    plantTypeID VARCHAR(5),
+    addedDate DATETIME
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS plants_database_table(
+    plantTypeID VARCHAR(5) NOT NULL PRIMARY KEY,
+    commonName VARCHAR(150),
+    scientificName VARCHAR(200),
+    height INT,
+    habit VARCHAR(20),
+    growth VARCHAR(1),
+    shade VARCHAR(3),
+    soil VARCHAR(3),
+    soilMoisture VARCHAR(4),
+    minTemp INT,
+    maxTemp INT,
+    minPH DECIMAL(2,1),
+    maxPH DECIMAL(2,1),
+    edible BOOLEAN
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS plants_status_table(
+    dataID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    plantID INT,
+    readTime DATETIME,
+    temperature DECIMAL(3, 2),
+    soilMoisture DECIMAL(3, 2),
+    waterLevel DECIMAL(3, 2),
+    lightIntensity DECIMAL(3, 2)
+) ENGINE = InnoDB DEFAULT CHARSET=UTF8MB4;

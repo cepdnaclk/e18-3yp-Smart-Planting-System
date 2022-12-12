3d21d66fc69571dfc9b4e927889f0b9f97f1ca1c
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-13 01:59:57 +0530

    Update user_profile.dart

--- smart_planting_app/lib/screens/user_profile.dart
+++ smart_planting_app/lib/screens/user_profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:smart_planting_app/screens/profile_widget.dart';
+import 'package:smart_planting_app/screens/user.dart';
 import 'package:smart_planting_app/screens/user_detail.dart';
 
 class profileScreen extends StatefulWidget {
@@ -14,7 +15,7 @@ class profileScreen extends StatefulWidget {
 }
 
 class _profileScreenState extends State<profileScreen> {
-  File? image;
+  late File image = File('asset/profile.png');
 
   Future pickImage(ImageSource source) async {
     try {
@@ -34,27 +35,113 @@ class _profileScreenState extends State<profileScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(color: Colors.black),
+        leading: const BackButton(color: Colors.black),
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
           IconButton(
               onPressed: () {},
-              icon: Icon(Icons.more_vert_rounded, color: Colors.black),
+              icon: const Icon(Icons.more_vert_rounded, color: Colors.black),
           )
         ],
       ),
       body: ListView(
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         children: [
           ProfileWidget(
             image: image,
             onClicked: (source) => pickImage(source),
-            imagePath: user.imagePath,
-          )
+            //imagePath: user.imagePath,
+          ),
+          const SizedBox(height: 20,),
+          buildName(user),
+          const SizedBox(height: 20,),
+          const StatWidget(),
+          const SizedBox(height: 20,),
+          buildAbout(user),
         ],
       ),
     );
   }
+
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.name,
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      ),
+      const SizedBox(height: 4,),
+      Text(
+        user.email,
+        style: const TextStyle(color: Colors.grey),
+      )
+    ],
+  );
+
+  Widget buildAbout(User user) => Container(
+    padding: const EdgeInsets.all(30),
+    child:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'About',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 2,),
+          Text(
+            user.about,
+            style: const TextStyle(),
+          )
+
+        ],
+      ),
+  );
 }
+
+class StatWidget extends StatelessWidget {
+  const StatWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) => Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      BuildButton(context, '9.9', 'Ranking'),
+      buildDivider(),
+      BuildButton(context, '45', 'Following'),
+      buildDivider(),
+      BuildButton(context, '5', 'Followers'),
+    ],
+  );
+
+
+  Widget BuildButton(BuildContext context, String value, String text) =>
+      MaterialButton(
+        padding: const EdgeInsets.all(8),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              value,
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 2,),
+            Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      );
+
+  Widget buildDivider() => Container(
+    height: 24,
+    child: const VerticalDivider(color: Colors.black,),
+  );
+
+}
+
 

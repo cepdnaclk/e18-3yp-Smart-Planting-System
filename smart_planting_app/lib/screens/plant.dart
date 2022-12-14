f18934fffd559afdd2ba7b46c4c32fe346dd415d
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-15 02:07:44 +0530

    Update plant.dart

--- smart_planting_app/lib/screens/plant.dart
+++ smart_planting_app/lib/screens/plant.dart
@@ -1,14 +1,14 @@
-class Plant{
-  final String imagePath;
-  final String name;
-  final String scientificName;
-  final String about;
-
-  User({
-    required this.imagePath,
-    required this.name,
-    required this.email,
-    required this.about,
-  });
-
-}
+// class Plant{
+//   final String imagePath;
+//   final String name;
+//   final String scientificName;
+//   final String about;
+//
+//   User({
+//     required this.imagePath,
+//     required this.name,
+//     required this.email,
+//     required this.about,
+//   });
+//
+// }

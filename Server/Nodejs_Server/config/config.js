8872cb9eb5f8dfabb1fcec8ff5154d49764adef0
anushanga pavith <anushangapavith2@gmail.com>
2022-12-18 04:37:33 +0530

    changed configs

--- Server/Nodejs_Server/config/config.js
+++ Server/Nodejs_Server/config/config.js
@@ -1,9 +1,9 @@
 // const dotenv = require('dotenv');
 
 module.exports = {
-    host: 'localhost',
-    user: 'root',
+    host: 'smart-plant-db-v1.cwancojjikuy.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
     // password: dotenv.config().parsed.DB_PASSWORD,
-    password: '',
+    password: 'Anu123ga',
     database: 'smart_planting_system'
 };

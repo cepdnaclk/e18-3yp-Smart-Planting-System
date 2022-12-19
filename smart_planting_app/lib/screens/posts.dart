6202f7db67b9941e59694832aaadd4d10c27141f
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2022-12-19 16:28:35 +0530

    Create posts.dart

---
+++ smart_planting_app/lib/screens/posts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class postWidget extends StatefulWidget {
+  const postWidget({Key? key}) : super(key: key);
+
+  @override
+  State<postWidget> createState() => _postWidgetState();
+}
+
+class _postWidgetState extends State<postWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

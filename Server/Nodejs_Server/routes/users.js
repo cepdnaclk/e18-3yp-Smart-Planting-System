5e217f5f39fda38e9b05e08c42023ef056013649
anushanga pavith <anushangapavith2@gmail.com>
2022-12-21 23:43:43 +0530

    Added some files

--- Server/Nodejs_Server/routes/users.js
+++ Server/Nodejs_Server/routes/users.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.post("/register", userController.register);
+router.get("/getUser", userController.getUserID);
 
 module.exports = router;

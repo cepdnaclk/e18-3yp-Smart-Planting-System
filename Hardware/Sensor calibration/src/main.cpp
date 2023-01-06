baa8b57d88760660a200c5ca48d051a21cad9a39
anushanga pavith <anushangapavith2@gmail.com>
2023-01-06 13:54:17 +0530

    Added wifi connectivity

--- Hardware/Sensor calibration/src/main.cpp
+++ Hardware/Sensor calibration/src/main.cpp
@@ -3,23 +3,106 @@
 #include <DallasTemperature.h>
 #include <SD.h>
 
-#define DEVICE_ID "01";
-const int SENSOR_PIN = A0; 				// All the analog read is connected to A0 pin of ESP8266 board
+#if defined(ESP32)
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#endif
 
-OneWire oneWire(SENSOR_PIN);         	// setup a oneWire instance
+const String DEVICE_ID = "01";
+const int SENSOR_PIN = A0; // All the analog read is connected to A0 pin of ESP8266 board
+
+OneWire oneWire(SENSOR_PIN);			// setup a oneWire instance
 DallasTemperature tempSensor(&oneWire); // pass oneWire to DallasTemperature library
 
-extern void soilMoistCalibration(int);
-extern void waterLevelCalibration(int);
+extern int soilMoistCalibration(int);
+extern int waterLevelCalibration(int);
 extern void temperatureRead(DallasTemperature tempSensor);
 
-void setup() {
+int soilMoistVal = 0;
+int waterLevelVal = 0;
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "Dialog_4G_336"
+#define WIFI_PASSWORD "56483cdD"
+
+// For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
+
+/* 2. Define the API Key */
+#define API_KEY "vBYe3iwFcnUvU8OOAKngy3Ne6aZTe53RArDxuOVC"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "test-hardware-project-default-rtdb.asia-southeast1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+void setup()
+{
 	Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
-	tempSensor.begin();    // initialize the temperature sensor
+	tempSensor.begin(); // initialize the temperature sensor
+	delay(2000);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.print("Connecting to Wi-Fi");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(300);
+	}
+	Serial.println();
+	Serial.print("Connected with IP: ");
+	Serial.println(WiFi.localIP());
+	Serial.println();
+
+	Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+	/* Assign the api key (required) */
+	config.api_key = API_KEY;
+
+	config.database_url = DATABASE_URL;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// Please make sure the device free Heap is not lower than 80 k for ESP32 and 10 k for ESP8266,
+	// otherwise the SSL connection will fail.
+	//////////////////////////////////////////////////////////////////////////////////////////////
+
+	Firebase.begin(DATABASE_URL, API_KEY);
+
+	// Comment or pass false value when WiFi reconnection will control by your code or third party library
+	// Firebase.reconnectWiFi(true);
+
+	Firebase.setDoubleDigits(5);
 }
 
-void loop() {
-    soilMoistCalibration(SENSOR_PIN);
-    // waterLevelCalibration(SENSOR_PIN);
+void loop()
+{
+	soilMoistVal = soilMoistCalibration(SENSOR_PIN);
+	// waterLevelCalibration(SENSOR_PIN);
 	// temperatureRead(tempSensor);
+	if (Firebase.ready())
+	{
+		// Firebase.setInt(fbdo, main, 5);
+		Firebase.setInt(fbdo, "/" + DEVICE_ID + "/soilMoisture", soilMoistVal);
+		Firebase.setInt(fbdo, "/" + DEVICE_ID + "/waterLevel", 10);
+		delay(200);
+
+		Serial.print("Soil moisture-- ");
+		Serial.println(soilMoistVal);
+
+		Serial.println();
+		Serial.println("------------------");
+		Serial.println();
+	}
+	delay(10000);
 }

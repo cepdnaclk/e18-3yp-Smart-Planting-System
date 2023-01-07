bd102a03ee1067d68fbe6e737f175aa25e60037a
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2023-01-07 09:25:17 +0530

    Create progress.dart

---
+++ smart_planting_app/lib/screens/progress.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+Container circularProgress() {
+  return Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.only(top: 10.0),
+    child: const CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.blue),
+    ),
+  );
+}
+
+Container linearProgress() {
+  return Container(
+    padding: const EdgeInsets.only(bottom: 10.0),
+    child: const LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.blue),
+    ),
+  );
+}

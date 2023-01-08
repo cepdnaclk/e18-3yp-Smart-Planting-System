fd6374c4b72bedc89adc9c7499a0941b953875c3
ChamaraDilshan <73582420+ChamaraDilshan@users.noreply.github.com>
2023-01-09 03:04:41 +0530

    Create custom_image.dart

---
+++ smart_planting_app/lib/screens/custom_image.dart
@@ -0,0 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+Widget cachedNetworkImage(String mediaUrl) {
+  return CachedNetworkImage(
+    imageUrl: mediaUrl,
+    fit: BoxFit.cover,
+    placeholder: (context, url) => Padding(
+      padding: EdgeInsets.all(20),
+      child: CircularProgressIndicator(),),
+    errorWidget: (context, url, error) => Icon(Icons.error),
+  );
+}

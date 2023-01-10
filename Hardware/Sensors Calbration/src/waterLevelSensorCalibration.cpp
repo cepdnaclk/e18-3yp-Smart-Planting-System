4a69bea40cb54e3628b7af8f8611a1e7a2441dda
anushanga pavith <anushangapavith2@gmail.com>
2023-01-10 19:16:32 +0530

    Added calibration of sensors

---
+++ Hardware/Sensors Calbration/src/waterLevelSensorCalibration.cpp
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+
+int waterLevelCalibration(int sensor_pin)
+{
+	int val;
+	val = analogRead(sensor_pin); // connect sensor to Analog 0
+	
+
+	int result = 1000;
+
+	if(val > 600) result = 0;
+	else if(val > 500) result = 1;
+	else if(val > 50) result = 2;
+	else result = 3;
+
+	Serial.println(val); // print the value to serial port
+
+	delay(500);
+	return result;
+}
+
+// 0-300 => level 3    -------- OK
+// 300-500 => level 2  -------- OK
+// 500-650 => level 1  -------- Low water warning
+// >650 => level 0     -------- Do not turn on pump!!
+
+
+// no water: <100
+// little water: 550
+// half water: 610
+// full water: 650
